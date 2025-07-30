5162ead96961404ade0f149946c582a86c2a9e74
Dominykas M. <26067386+Tasty-Kiwi@users.noreply.github.com>
2025-07-30 15:07:36 +0300

    make lambdas callable

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 KiwiLang is a Lisp-like programming language written in Python.
+
+Currently, it uses JSON as its syntax, but it will be changed to KiwiLang's own syntax in the future.
+
+## Usage
+
+```bash
+python main.py test.json [-v] [-d]
+```
+
+- `-v` or `--verbose` - verbose output
+- `-d` or `--debug` - debug output
+
+## Syntax
+
+```json
+[
+  ["print", "Hello, World!"],
+  ["defn", ":x", 2],
+  ["print", ":x"],
+  ["defn", ":my_list", ["list", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]],
+  ["print", ":my_list"],
+
+  ["defn", ":y", 2],
+  ["if", ["=", ":y", 2], 
+    ["print", "y is 2"],
+    ["print", "y is not 2"]
+  ],
+
+  ["print", ["range", 1, 10]],
+
+  ["for", "$i", ["range", 1, 10], ["print", "$i"]],
+  
+  ["defn", ":test", ["lambda", ["$x"], ["print", "$x"]]],
+  [":test", "Hello, test function!"]
+]
+```
+
+## Features
+
+- Variables
+- Lambda functions
+- Control flow
+- Loops
+- Lists
--- main.py
+++ main.py
@@ -7,17 +7,25 @@ parser.add_argument("file", type=str)
 parser.add_argument("-v", "--verbose", action="store_true")
 parser.add_argument("-d", "--debug", action="store_true")
 
-args = parser.parse_args()
+app_args = parser.parse_args()
 global_variables: dict[str, Any] = {}
 
+def strip_variable_sigil(name: str) -> str:
+    if name.startswith(":"):
+        return name[1:]
+    elif name.startswith("$"):
+        return name[1:]
+    else:
+        return name
+
 def evaluate_node(node: Any, local_vars: dict[str, Any] = {}) -> Any:
-    global args
+    global app_args
     global global_variables
 
-    if args.verbose:
+    if app_args.verbose:
         print("Evaluating node: ", node, "Local variables: ", local_vars)
 
-    if args.debug and local_vars:
+    if app_args.debug and local_vars:
         print("Local variables: ", local_vars)
 
     match node:
@@ -38,20 +46,21 @@ def evaluate_node(node: Any, local_vars: dict[str, Any] = {}) -> Any:
         case ["print", value]:
             print(evaluate_node(value, local_vars))
         case ["defn", name, value] if name.startswith(":"):
-            global_variables[name[1:]] = evaluate_node(value, local_vars)
+            global_variables[strip_variable_sigil(name)] = evaluate_node(value, local_vars)
         case ["defn", name, value] if name.startswith("$"):
-            local_vars[name[1:]] = evaluate_node(value, local_vars)
+            if strip_variable_sigil(name) in global_variables or strip_variable_sigil(name) in local_vars:
+                raise Exception(f"Variable {name} is already defined")
+            local_vars[strip_variable_sigil(name)] = evaluate_node(value, local_vars)
         case ["for", name, iterable, body] if name.startswith("$"):
             for item in evaluate_node(iterable, local_vars):
-                local_vars[name[1:]] = item
+                local_vars[strip_variable_sigil(name)] = item
                 evaluate_node(body, local_vars)
-        # TODO: Fix this
-        case ["lambda", arguments, body]:
-            print("Lambda: ", arguments, body)
-            print(**arguments)
-            return lambda **arguments: evaluate_node(body, local_vars | arguments)
+        # TODO: add $ char recognition
+        case ["lambda", arguments, *bodies]:
+            arguments = [strip_variable_sigil(arg) for arg in arguments]
+            return lambda *args : [evaluate_node(body, local_vars | dict(zip(arguments, args))) for body in bodies]
         case ["list", *items]:
-            return [evaluate_node(item, local_vars) for item in items]
+            return tuple(evaluate_node(item, local_vars) for item in items)
         case ["if", condition, truthy]:
             if evaluate_node(condition, local_vars):
                 return evaluate_node(truthy, local_vars)
@@ -66,31 +75,31 @@ def evaluate_node(node: Any, local_vars: dict[str, Any] = {}) -> Any:
             return tuple(range(evaluate_node(start, local_vars), evaluate_node(end, local_vars)))
         case ["range", end]:
             return tuple(range(evaluate_node(end, local_vars)))
-        # TODO: Fix this
         case [function, *arguments]:
-            return evaluate_node(function, local_vars)(*[evaluate_node(arg, local_vars) for arg in arguments])
+            if global_variables.get(strip_variable_sigil(function)):
+                return global_variables[strip_variable_sigil(function)](*arguments)
+            else:
+                raise Exception(f"Unable to call {function}")
         case _:
             if isinstance(node, str) and node.startswith(":"):
-                if node[1:] in global_variables:
-                    return global_variables[node[1:]]
+                if strip_variable_sigil(node) in global_variables:
+                    return global_variables[strip_variable_sigil(node)]
                 else:
-                    raise Exception(f"Variable {node[1:]} not found")
+                    raise Exception(f"Variable {node} is not defined")
             elif isinstance(node, str) and node.startswith("$"):
-                if node[1:] in local_vars:
-                    return local_vars[node[1:]]
+                if strip_variable_sigil(node) in local_vars:
+                    return local_vars[strip_variable_sigil(node)]
                 else:
-                    raise Exception(f"Local variable {node[1:]} not found")
+                    raise Exception(f"Variable {node} is not defined")
             else:
                 return node
 
 def map_variable(name: str, value: Any):
     global_variables[name] = value
 
-with open(args.file, "r") as file:
+with open(app_args.file, "r") as file:
     data = json.load(file)
-    map_variable("test", lambda x: x + 1)
     for node in data:
         evaluate_node(node)
-    global_variables["add"](1, 2)
-    if args.debug:
+    if app_args.debug:
         print(f"\n======\nDebug information:\nGlobal variables: {global_variables}\n======")
